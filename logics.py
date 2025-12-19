20b03eb9ccbabb347110fbae8deb5c2b05f1dd54
HiteshMaharana <artegooge@gmail.com>
2025-12-19 09:12:15 +0530

    Add logics.py with initial function implementations
    Initial commit of logics.py with commented code for various functions including hero list, factorial calculation, and currency converter.

---
+++ logics.py
@@ -0,0 +1,42 @@
+# heros = ["Superman","hanu-man","batman","ironman","spiderman","captain america"]
+# print(heros)
+
+# def print_heros(list):
+#     print(len(list))       ----> lenght find using function
+
+
+#------------factorial----------
+# i=1
+# n=4
+# while i<=10:
+#     # mul = n*i
+#     # print(n,"*",i,"=",mul)
+#     i+=1
+
+# n=5
+# fact =1
+# for i in range(1,n+1):
+#     fact = fact*i
+# print(fact)
+# n=int(input("Enter a number: "))
+
+# def cal_fact(n):
+#     fact = 1
+
+#     for i in range(1,n+1):
+#         fact *= i
+#     print(fact)
+
+# cal_fact(5)       -------> using funtion 
+
+#----------- Curency converter ------
+
+# def converter(usd_val):
+#     inr_val = usd_val * 83
+#     print(usd_val,"USD =",inr_val,"INR")
+
+# converter(2)       --------> using function
+
+
+
+
